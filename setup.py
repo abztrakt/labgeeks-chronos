f210da518ce8fffea304fd0f47825ddba6a05542
Jon Swanson <jswan2@uw.edu>
2012-12-18 15:54:19 -0800

    made labgeeks_chronos an egg (labgeeks-chronos) for buildout

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name = 'labgeeks-chronos',
+    version = '1.0',
+    license = 'Apache',
+    url = 'http://github.com/abztrakt/labgeeks_chronos',
+    description = 'The punchclock/timekeeping app for the labgeeks staff management tool.',
+    author = 'Craig Stimmel',
+    packages = find_packages(),
+    install_requires = [
+        'setuptools',
+        'labgeeks-people',
+        'South==0.7.3',
+    ],
+)
