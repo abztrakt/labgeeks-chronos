ebe49ab7bd4231bc9dc2b3f277208927d5ecc361
ALi Haugh <haughar@hotmail.com>
2014-11-12 12:08:20 -0800

    Added test cases for the Late Table. test_similar_shifts will not pass because it is a bug that was being tested for and is going to be fixed. Fixes SDEV-481.

--- labgeeks_chronos/tests.py
+++ labgeeks_chronos/tests.py
@@ -6,6 +6,8 @@ from django.contrib.auth import login, logout
 from django.contrib.auth.models import User
 from labgeeks_chronos import models as c_models
 from labgeeks_chronos import views as c_views
+from labgeeks_chronos import utils as c_utils
+import unittest
 
 
 class StartTestCase(TestCase):
@@ -14,7 +16,7 @@ class StartTestCase(TestCase):
     """
     def setUp(self):
 
-        #Create users and set permissions
+        # Create users and set permissions
         self.ryu = User.objects.create(username='Ryu', password='hadouken', email='ryu@streetfighter.com')
         self.ken = User.objects.create(username='Ken', password='shoryuken', email='ken@streetfighter.com')
         self.akuma = User.objects.create(username='Akuma', password='shun goku satsu', email='akuma@streetfighter.com')
@@ -35,24 +37,24 @@ class StartTestCase(TestCase):
         self.akuma.is_staff = False
         self.akuma.is_superuser = False
 
-        #Create locations. These are used for clocking in and out
+        # Create locations. These are used for clocking in and out
         self.loc1 = c_models.Location.objects.create(name='Japan')
         self.loc2 = c_models.Location.objects.create(name='America')
         self.loc3 = c_models.Location.objects.create(name='Japan')
 
-        #Ip addresses, used for verifying punchclock locations
+        # Ip addresses, used for verifying punchclock locations
         self.ip1 = '000.000.00.00'
         self.ip2 = '123.456.78.90'
         self.ip3 = '111.222.33.44'
 
-        #Punch clocks, where people can clock in.
+        # Punch clocks, where people can clock in.
         self.pc1 = c_models.Punchclock.objects.create(name='Rooftop', location=self.loc1, ip_address=self.ip1)
         self.pc2 = c_models.Punchclock.objects.create(name='Harbor', location=self.loc2, ip_address=self.ip2)
         self.pc3 = c_models.Punchclock.objects.create(name='Temple', location=self.loc3, ip_address=self.ip3)
-        #Imitate a HTMLrequest object
+        # Imitate a HTMLrequest object
         self.request = self.client
 
-        #Save the fields to the test db to imitate the live site data.
+        # Save the fields to the test db to imitate the live site data.
         self.ryu.save()
         self.ken.save()
         self.akuma.save()
@@ -69,13 +71,13 @@ class ModelsTestCase(StartTestCase):
     Test the models for Chronos. Add / edit any test.
     """
     def test_location(self):
-        #Should be true
+        # Should be true
         self.assertEqual(self.loc1.name, 'Japan')
         self.assertEqual(self.loc2.name, 'America')
         self.assertEqual(self.loc3.name, 'Japan')
 
     def test_punchclock(self):
-        #Should be true
+        # Should be true
         self.assertEqual(self.pc1.location.name, 'Japan')
         self.assertNotEqual(self.pc2.location, 'Japan')
         self.assertIsNotNone(c_models.Punchclock.objects.get(name='Rooftop'))
@@ -86,7 +88,7 @@ class ClockInClockOutTestCase(StartTestCase):
     Test if users are properly clocking in and out.
     """
     def test_clockin(self):
-        #self.request.method = 'POST'
+        # self.request.method = 'POST'
         pass
 
 
@@ -112,3 +114,113 @@ class ShiftsTestCase(StartTestCase):
 
         self.assertIsNotNone(c_views.get_shifts(year, month, day, user, week, payperiod))
         self.assertIsNotNone(c_views.get_shifts(year, month, None, None, None, None))
+
+
+class LateTableCase(TestCase):
+    """
+    Test that the late table is receiving correct information to display.
+    """
+    def test_on_time(self):
+        from mock import patch
+
+        chronos = []
+        pclock = {'comm_in': u'IN: ', 'netid': u'user1', 'shift': 25294, 'punchclock_in_location': u'Odegaard Help Desk', 'name': u'User 1', 'in': '11:30:27', 'comm_out': u'OUT: ', 'out': '14:45:37'}
+        chronos.append(pclock)
+
+        date = '1927-11-04'
+
+        service = 'dummy_service'
+
+        with patch.object(c_utils, 'read_api', return_value={"Shifts": {"user1": [{"Out": "14:45:00", "In": "11:30:00", "Shift": 1}]}}):
+            results = c_utils.compare(chronos, date, service)
+
+        expected_conflicts = [{'name': u'User 1', 'netid': 'user1'}]
+
+        self.assertEqual(results, ([], expected_conflicts))
+
+    def test_slighlty_early(self):
+
+        from mock import patch
+        import datetime
+
+        chronos = []
+        pclock = {'comm_in': u'IN: ', 'netid': u'user1', 'shift': 25294, 'punchclock_in_location': u'Odegaard Help Desk', 'name': u'User 1', 'in': '11:28:27', 'comm_out': u'OUT: ', 'out': '14:45:37'}
+        chronos.append(pclock)
+
+        date = '1927-11-03'
+
+        service = 'dummy_service'
+
+        with patch.object(c_utils, 'read_api', return_value={"Shifts": {"user1": [{"Out": "14:45:00", "In": "11:30:00", "Shift": 1}]}}):
+            results = c_utils.compare(chronos, date, service)
+
+        expected_conflicts = [{'clock_in': '11:28:27', 'sched_out': '14:45:00', 'clock_out': '14:45:37', 'comm_out': u'OUT: ', 'sched_in': '11:30:00', 'netid': 'user1', 'diff_in_early': datetime.timedelta(0, 93), 'name': u'User 1', 'comm_in': u'IN: '}]
+
+        self.assertEquals(results, ([], expected_conflicts))
+
+    def test_slightly_late(self):
+        from mock import patch
+        import datetime
+
+        chronos = []
+
+        pclock = {'comm_in': u'IN: ', 'netid': u'user1', 'shift': 25294, 'punchclock_in_location': u'Odegaard Help Desk', 'name': u'User 1', 'in': '11:30:27', 'comm_out': u'OUT: ', 'out': '14:46:37'}
+        chronos.append(pclock)
+
+        date = '1927-03-11'
+
+        service = 'dummy_service'
+
+        with patch.object(c_utils, 'read_api', return_value={"Shifts": {"user1": [{"Out": "14:45:00", "In": "11:30:00", "Shift": 1}]}}):
+            results = c_utils.compare(chronos, date, service)
+
+        expected_conflicts = [{'clock_in': '11:30:27', 'sched_in': '11:30:00', 'diff_out_late': datetime.timedelta(0, 97), 'comm_in': u'IN: ', 'clock_out': '14:46:37', 'netid': 'user1', 'name': u'User 1', 'sched_out': '14:45:00', 'comm_out': u'OUT: '}]
+
+        self.assertEqual(results, ([], expected_conflicts))
+
+    def test_similar_shifts(self):
+        from mock import patch
+        import datetime
+
+        chronos = []
+
+        # This shift was worked the day before the day being examined--date = '1927-03-11'
+        pclock = {'comm_in': u'IN: ', 'netid': u'user1', 'shift': 25291, 'punchclock_in_location': u'Odegaard Help Desk', 'name': u'User 1', 'in': '18:49:20', 'comm_out': u'OUT: ', 'out': '22:21:25'}
+
+        chronos.append(pclock)
+        pclock = {}
+
+        # This shift was worked on date being examined--date = '1927-03-11'
+        pclock = {'comm_in': u'IN: ', 'netid': u'user1', 'shift': 25261, 'punchclock_in_location': u'Odega    ard Help Desk', 'name': u'User 1', 'in': '09:12:41', 'comm_out': u'OUT: ', 'out': '14:02:06'}
+        chronos.append(pclock)
+
+        date = '1927-03-11'
+        service = 'dummy_service'
+
+        # This shift was supposed to be worked on date being examined
+        with patch.object(c_utils, 'read_api', return_value={"Shifts": {"user1": [{"Out": "22:15:00", "In": "20:45:00", "Shift": 1}]}}):
+            request = c_utils.compare(chronos, date, service)
+
+        expected_conflict = []
+
+        expected_no_show = [{'In': '20:45:00', 'Out': '22:15:00', 'Shift': 1, 'netid': 'user1'}]
+
+        self.assertEqual(request, (expected_no_show, expected_conflict))
+
+    def test_no_show(self):
+        from mock import patch
+        import datetime
+
+        chronos = []
+
+        date = '1927-03-11'
+
+        service = 'dummy_service'
+
+        with patch.object(c_utils, 'read_api', return_value={"Shifts": {"user1": [{"Out": "14:45:00", "In": "11:30:00", "Shift": 1}]}}):
+            results = c_utils.compare(chronos, date, service)
+
+        expected_conflicts = []
+        expected_no_shows = [{'In': '11:30:00', 'Out': '14:45:00', 'Shift': 1, 'netid': 'user1'}]
+
+        self.assertEqual(results, (expected_no_shows, expected_conflicts))
