9412297b56e33af05734384402247b611fc5c545
ALi Haugh <haughar@hotmail.com>
2014-11-12 16:53:18 -0800

    Adjusted the tests to work with bug fix to late table. Fixing SDEV-538

--- labgeeks_chronos/tests.py
+++ labgeeks_chronos/tests.py
@@ -93,10 +93,10 @@ class ClockInClockOutTestCase(StartTestCase):
 
 
 class ShiftsTestCase(StartTestCase):
-
     """ Test shifts. Make sure we are grabbing the right ones. Either create
     shifts or use ones from the db.
     """
+
     def setUp(self):
         super(ShiftsTestCase, self).setUp()
         person = self.ryu
@@ -120,107 +120,129 @@ class LateTableCase(TestCase):
     """
     Test that the late table is receiving correct information to display.
     """
+
+    def setUp(self):
+        user1 = User.objects.create_user('user1', 'user1@uw.edu', 'coolestuser')
+        user1.name = 'User 1'
+        user1.is_active = True
+        user1.is_staff = True
+        user.is_superuser = False
+        user1.save()
+        location = Location.objects.create()
+        location.name = 'Campus'
+        pclock = Punchclock.objects.create(name='ode', location=location, ip_address='0.0.0.0')
+
     def test_on_time(self):
         from mock import patch
 
-        chronos = []
-        pclock = {'comm_in': u'IN: ', 'netid': u'user1', 'shift': 25294, 'punchclock_in_location': u'Odegaard Help Desk', 'name': u'User 1', 'in': '11:30:27', 'comm_out': u'OUT: ', 'out': '14:45:37'}
-        chronos.append(pclock)
-
+        shift = Shift.objects.create(person=user1, intime=datetime.datetime(1927, 11, 04, 11, 30, 27), outtime=datetime.datetime(1927, 11, 04, 14, 45, 37), shiftnote='IN: OUT: ', in_clock=pclock, out_clock=pclock)
+        shift.save()
         date = '1927-11-04'
-
         service = 'dummy_service'
 
         with patch.object(c_utils, 'read_api', return_value={"Shifts": {"user1": [{"Out": "14:45:00", "In": "11:30:00", "Shift": 1}]}}):
-            results = c_utils.compare(chronos, date, service)
+            results = c_utils.compare(date, service)
 
         expected_conflicts = [{'name': u'User 1', 'netid': 'user1'}]
 
         self.assertEqual(results, ([], expected_conflicts))
+        shift.delete()
 
     def test_slighlty_early(self):
-
         from mock import patch
         import datetime
 
-        chronos = []
-        pclock = {'comm_in': u'IN: ', 'netid': u'user1', 'shift': 25294, 'punchclock_in_location': u'Odegaard Help Desk', 'name': u'User 1', 'in': '11:28:27', 'comm_out': u'OUT: ', 'out': '14:45:37'}
-        chronos.append(pclock)
-
+        shift = Shift.objects.create(person=user1, intime=datetime.datetime(1927, 11, 03, 11, 28, 27), outtime=datetime.datetime(1927, 11, 03, 14, 45, 37), shiftnote='IN: OUT: ', in_clock=pclock, out_clock=pclock)
+        shift.save()
         date = '1927-11-03'
-
         service = 'dummy_service'
 
         with patch.object(c_utils, 'read_api', return_value={"Shifts": {"user1": [{"Out": "14:45:00", "In": "11:30:00", "Shift": 1}]}}):
-            results = c_utils.compare(chronos, date, service)
+            results = c_utils.compare(date, service)
 
         expected_conflicts = [{'clock_in': '11:28:27', 'sched_out': '14:45:00', 'clock_out': '14:45:37', 'comm_out': u'OUT: ', 'sched_in': '11:30:00', 'netid': 'user1', 'diff_in_early': datetime.timedelta(0, 93), 'name': u'User 1', 'comm_in': u'IN: '}]
 
         self.assertEquals(results, ([], expected_conflicts))
+        shift.delete()
 
     def test_slightly_late(self):
         from mock import patch
         import datetime
 
-        chronos = []
-
-        pclock = {'comm_in': u'IN: ', 'netid': u'user1', 'shift': 25294, 'punchclock_in_location': u'Odegaard Help Desk', 'name': u'User 1', 'in': '11:30:27', 'comm_out': u'OUT: ', 'out': '14:46:37'}
-        chronos.append(pclock)
-
-        date = '1927-03-11'
-
+        shift = Shift.objects.create(person=user1, intime=datetime.datetime(1927, 11, 03, 11, 30, 27), outtime=datetime.datetime(1927, 11, 03, 14, 46, 37), shiftnote='IN: OUT: ', in_clock=pclock, out_clock=pclock)
+        shift.save()
+        date = '1927-11-03'
         service = 'dummy_service'
 
         with patch.object(c_utils, 'read_api', return_value={"Shifts": {"user1": [{"Out": "14:45:00", "In": "11:30:00", "Shift": 1}]}}):
-            results = c_utils.compare(chronos, date, service)
+            results = c_utils.compare(date, service)
 
         expected_conflicts = [{'clock_in': '11:30:27', 'sched_in': '11:30:00', 'diff_out_late': datetime.timedelta(0, 97), 'comm_in': u'IN: ', 'clock_out': '14:46:37', 'netid': 'user1', 'name': u'User 1', 'sched_out': '14:45:00', 'comm_out': u'OUT: '}]
 
         self.assertEqual(results, ([], expected_conflicts))
+        shift.delete()
 
     def test_similar_shifts(self):
         from mock import patch
         import datetime
 
-        chronos = []
-
         # This shift was worked the day before the day being examined--date = '1927-03-11'
-        pclock = {'comm_in': u'IN: ', 'netid': u'user1', 'shift': 25291, 'punchclock_in_location': u'Odegaard Help Desk', 'name': u'User 1', 'in': '18:49:20', 'comm_out': u'OUT: ', 'out': '22:21:25'}
-
-        chronos.append(pclock)
-        pclock = {}
+        shift1 = Shift.objects.create(person=user1, intime=datetime.datetime(1927, 11, 02, 18, 49, 20), outtime=datetime.datetime(1927, 11, 02, 22, 21, 25), shiftnote='IN: OUT: ', in_clock=pclock, out_clock=pclock)
 
         # This shift was worked on date being examined--date = '1927-03-11'
-        pclock = {'comm_in': u'IN: ', 'netid': u'user1', 'shift': 25261, 'punchclock_in_location': u'Odega    ard Help Desk', 'name': u'User 1', 'in': '09:12:41', 'comm_out': u'OUT: ', 'out': '14:02:06'}
-        chronos.append(pclock)
-
-        date = '1927-03-11'
+        shift2 = Shift.objects.create(person=user1, intime=datetime.datetime(1927, 11, 03, 09, 12, 41), outtime=datetime.datetime(1927, 11, 03, 14, 02, 06), shiftnote='IN: OUT: ', in_clock=pclock, out_clock=pclock)
+        shift1.save()
+        shift2.save()
+        date = '1927-11-03'
         service = 'dummy_service'
 
         # This shift was supposed to be worked on date being examined
         with patch.object(c_utils, 'read_api', return_value={"Shifts": {"user1": [{"Out": "22:15:00", "In": "20:45:00", "Shift": 1}]}}):
-            request = c_utils.compare(chronos, date, service)
+            request = c_utils.compare(date, service)
 
         expected_conflict = []
 
         expected_no_show = [{'In': '20:45:00', 'Out': '22:15:00', 'Shift': 1, 'netid': 'user1'}]
 
         self.assertEqual(request, (expected_no_show, expected_conflict))
+        shift1.delete()
+        shift2.delete()
 
     def test_no_show(self):
         from mock import patch
         import datetime
 
-        chronos = []
-
+        shift = Shift.objects.create(person=user1, intime=datetime.datetime(1927, 11, 03, 11, 30, 27), outtime=datetime.datetime(1927, 11, 03, 14, 46, 37), shiftnote='IN: OUT: ', in_clock=pclock, out_clock=pclock)
+        shift.save()
         date = '1927-03-11'
-
         service = 'dummy_service'
 
         with patch.object(c_utils, 'read_api', return_value={"Shifts": {"user1": [{"Out": "14:45:00", "In": "11:30:00", "Shift": 1}]}}):
-            results = c_utils.compare(chronos, date, service)
+            results = c_utils.compare(date, service)
 
         expected_conflicts = []
         expected_no_shows = [{'In': '11:30:00', 'Out': '14:45:00', 'Shift': 1, 'netid': 'user1'}]
 
         self.assertEqual(results, (expected_no_shows, expected_conflicts))
+
+    def test_interpet_results(self):
+        from mock import patch
+        import datetime
+
+        shift = Shift.objects.create(person=user1, intime=datetime.datetime(1927, 11, 03, 11, 30, 27), outtime=datetime.datetime(1927, 11, 03, 14, 46, 37), shiftnote='IN: OUT: ', in_clock=pclock, out_clock=pclock)
+
+        shift.save()
+        date = '1927-03-11'
+        service = 'dummy_service'
+
+        with patch.object(c_utils, 'read_api', return_value={"Shifts": {"user1": [{"Out": "14:45:00", "In": "11:30:00", "Shift": 1}]}}):
+            msg = c_utils.interpet_results(chronos, date, service)
+
+        expected_msg = [{'status': 'Clock Out Late', 'comm_in': u'IN: ', 'color': 'blacker', 'sched_in': '11:30 AM', 'clock_out': '02:46 PM', 'date': '1927-03-11', 'change': datetime.timedelta(0, 97), 'comm_out': u'OUT: ', 'clock_in': '11:30 AM', 'name': u'User 1', 'netid': 'user1', 'sched_out': '02:45 PM'}]
+
+        self.assertEqual(msg, expected_msg)
+
+    def breakDown(self):
+        user1.delete()
+        location.delete()
+        pclock.delete()
